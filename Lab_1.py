8f24c7a2f600d67231f7ddd1bc1b451b52a64fd7
jonathanko45 <39708300+jonathanko45@users.noreply.github.com>
2022-09-25 19:01:14 -0400

    Update Lab_1.py

--- Lab_1.py
+++ Lab_1.py
@@ -1,2 +1,29 @@
+import random
+import math
+
 print("unga bunga pom")
 print("unga bunga pom1")
+
+#uniform random variable
+U = random.uniform(0, 1)
+
+
+lambda1 = 75  #rate parameter, Average number of packets generated /arrived (packets per second)
+L =           #Average length of a packet in bits.
+alpha =       #Average number of observer events per second
+C =           #The transmission rate of the output link in bits per second.
+rho =         #Utilization of the queue (= input rate/service rate = L λ/C)
+E[N] =        #Average number of packets in the buffer/queue
+P_idle =      #The proportion of time the server is idle, i.e., no packets in the queue nor a packet is being transmitted.
+P_loss =      #The packet loss probability (for M/M/1/K queue). It is the ratio of the total number of packets lost due to buffer full condition to the total number of generated packets
+
+
+#exponential random variable
+x = -(1/lambda1) * math.log(1 - U) 
+
+def M/M/1():
+  
+  
+  
+def M/M/1/K():
+  
