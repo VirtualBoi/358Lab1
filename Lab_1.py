48bd00381abdb2c2e7101a5f05197fd58f226f54
Ethan <gilbertethan.projects@gmail.com>
2022-09-27 20:12:52 -0400

    Added stuff (mid day today)

--- Lab_1.py
+++ Lab_1.py
@@ -7,12 +7,12 @@ lambda1 = 75  #rate parameter, Average number of packets generated /arrived (pac
 L = 2000          #Average length of a packet in bits.
 C = 1000000         #The transmission rate of the output link in bits per second.
 rho = 0         #Utilization of the queue (= input rate/service rate = L λ/C)
+alpha = 60/((1/lambda1)/5)      #Average number of observer events per second
 
 #
-alpha = 0      #Average number of observer events per second
-E_N = 0         #Average number of packets in the buffer/queue
-P_idle = 0      #The proportion of time the server is idle, i.e., no packets in the queue nor a packet is being transmitted.
-P_loss = 0      #The packet loss probability (for M/M/1/K queue). It is the ratio of the total number of packets lost due to buffer full condition to the total number of generated packets
+E_N = 0                         #Average number of packets in the buffer/queue
+P_idle = 0                      #The proportion of time the server is idle, i.e., no packets in the queue nor a packet is being transmitted.
+P_loss = 0                      #The packet loss probability (for M/M/1/K queue). It is the ratio of the total number of packets lost due to buffer full condition to the total number of generated packets
 
 class packet:
     RV_A_time = 0
@@ -27,11 +27,12 @@ def rand_return():
     return -(1/lambda1) * math.log(1 - U) #exponential random variable
 
 
-def no_queue():
-    print ("NO QUEUE")
+def DES(size, T): #funciton holding the main DES algorithm 
+    tick_count = 0
+    while (tick_count <= T):
+        packet_time = rand_return()
+        tick_count+=packet_time
 
-def infinite_queue():
-    print ("INFINITE QUEUE")
 
 def question_1():
     count = 1000
@@ -44,9 +45,11 @@ def question_1():
         mean += rand_num
         var = var + pow(rand_num - 1/lambda1, 2) 
         count -= 1
-    print ("mean: ", mean/1000)
+    print ("Expected mean: ", 1/lambda1)
+    print ("Mean: ", mean/1000)
+    print ("Expected variance: ", 1/(lambda1**2))
     print ("Variance",  var/1000)
 
-question_1()    
-#no_queue()
-#infinite_queue()
+#question_1()    
+#DES(math.inf, 2)
+#DES(X, 2)
