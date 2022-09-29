f124ef35c1c6b85343c4a5cc9459e3e00674e039
Ethan <gilbertethan.projects@gmail.com>
2022-09-29 16:51:48 -0400

    Did a bunch of stuff

--- Lab_1.py
+++ Lab_1.py
@@ -1,7 +1,6 @@
 import random
 import math
 
-
 #Defined by Qiuestion
 lambda1 = 75  #rate parameter, Average number of packets generated /arrived (packets per second)
 L = 2000          #Average length of a packet in bits.
@@ -9,33 +8,69 @@ C = 1000000         #The transmission rate of the output link in bits per second
 rho = 0         #Utilization of the queue (= input rate/service rate = L λ/C)
 alpha = 60/((1/lambda1)/5)      #Average number of observer events per second
 
-#
+queue = []
+
 E_N = 0                         #Average number of packets in the buffer/queue
 P_idle = 0                      #The proportion of time the server is idle, i.e., no packets in the queue nor a packet is being transmitted.
 P_loss = 0                      #The packet loss probability (for M/M/1/K queue). It is the ratio of the total number of packets lost due to buffer full condition to the total number of generated packets
 
 class packet(object):
-    def __init__(RV_A_time, RV_size):
-        RV_A_time = RV_A_time
-        RV_size = RV_size
-    
-    #calculate and populate the bellow variables on creation of packet
+     #calculate and populate the bellow variables on creation of packet
+     def __init__(self, RV_A_length, RV_length, tick_count):
+        self.RV_A_length = RV_A_length #random time taken to arrive
+        self.RV_length = RV_length #random length of packet
+        self.A_time = tick_count + RV_A_length #Arrival time
+        self.service_length = RV_length/C #Service time
+        self.D_time = 0 #Departure time
 
-    size = 0 #size in bits
-    A_time = 0 #Arrival time
-    S_time = 0 #Service time
-    D_time = 0 #Departure time
+def rand_return(tick_count):
+    U = random.uniform(0, 1) #uniform random variable (for arrival time)
+    V = random.uniform(0, 1) #uniform random variable (for size)
+    return packet(-(1/lambda1) * math.log(1 - U), -(1/(1/L)) * math.log(1 - V), tick_count)
 
-def rand_return():
-    U = random.uniform(0, 1) #uniform random variable
-    return -(1/lambda1) * math.log(1 - U) #exponential random variable
-
-
-def DES(size, T): #funciton holding the main DES algorithm 
+def DES(size, T): #funciton holding the main DES algorithm T = 
     tick_count = 0
-    while (tick_count <= T):
-        packet_time = rand_return()
-        tick_count+=packet_time
+    tick_increment = 0.00001
+    current_packet = rand_return(0)
+
+    while tick_count <= T:
+        if arrival_check(tick_count, current_packet):
+            add_to_queue(current_packet)
+            current_packet = rand_return(tick_count)
+
+        departure_check(tick_count, current_packet)
+        observer_check(tick_count)
+        tick_count += tick_increment
+
+        #calculate E[N], P_idle, P_loss
+        if not queue:
+            P_idle += tick_increment
+
+
+def arrival_check(tick_count, packet):
+    if tick_count == packet.A_time:
+        print ("ARRIVED")
+        return True
+
+    return False
+
+def departure_check(tick_count, packet):
+    if tick_count == packet.D_time:
+        print ("DEPARTED")
+
+def observer_check(tick_count):
+    if tick_count % ((1/lambda1)/5):
+        print ("CHECk")
+
+def add_to_queue(packet, tick_count):
+    sum = 0
+    queue.append(packet)
+
+    for p in queue:
+        sum += p.service_length
+    sum += tick_count
+
+    queue[-1].D_time = sum
 
 def question_1():
     count = 1000
@@ -43,7 +78,7 @@ def question_1():
     var = 0
     rand_num = 0
     queue = 0
-    while(count > 1):
+    while count > 1:
         rand_num = rand_return()
         mean += rand_num
         var = var + pow(rand_num - 1/lambda1, 2) 
